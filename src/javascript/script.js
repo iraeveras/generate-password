ffc5637e79c5e2f1187da6b0e2cddc6a8c151584
iraeveras <irae.online@gmail.com>
2024-10-23 22:26:14 -0300

    finalization

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -29,7 +29,8 @@ function getPasswordSize() {
     const size = document.querySelector('#size').value;
 
     if (isNaN(size) || size < 4 || size > 128) {
-        alert('Tamanho inválid, digite um número entre 4 e 128')
+        message('Tamanho inválid, digite um número entre 4 e 128!', 'danger');
+        return
     }
 
     return size
@@ -51,12 +52,37 @@ function generatePassword(size, chartTypes) {
     return passwordGenerate
 }
 
+function message(text, status = 'success') {
+    Toastify({
+        text: text,
+        duration: 2000,
+        style: {
+            background: status === 'success' ? '#84cc16' : '#dc2626',
+            boxShadow: 'none'
+        }
+    }).showToast();
+}
+
 document.querySelector('#generate').addEventListener('click', function () {
     const size = getPasswordSize();
     const charTypes = getChartTypes();
+
+    if (!size) {
+        return
+    }
+
+    if (!charTypes.length) {
+        message('Selecione pelo menos um tipo de caractere.', 'danger');
+        return
+    }
+
     const passwordGenerated = generatePassword(size, charTypes);
 
     document.querySelector('#password-container').classList.add('show');
     document.querySelector('#password').textContent = passwordGenerated;
+});
 
+document.querySelector('#copy').addEventListener('click', function () {
+    navigator.clipboard.writeText(document.querySelector('#password').textContent);
+    message('Senha copiada com sucesso!', 'success');
 })
